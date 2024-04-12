074dddad795b945c749ffd03a0563818b4969ab2
wxqner <143152895+wxqner@users.noreply.github.com>
2024-04-11 23:18:33 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/*********************************************************************************
+
+	Template Name: Template Full Name Here
+	Description: A perfect template to build beautiful and unique medical websites. It comes with nice and clean design.
+	Version: 1.0
+
+	Note: This is main js.
+
+**********************************************************************************/
+
+/**************************************************************
+	
+	STYLESHEET INDEXING
+	|
+	|
+	|___ Activation List
+	|
+	|
+	|___ END STYLESHEET INDEXING
+
+***************************************************************/
+
+
+(function($){
+    'use strict';
+    
+
+    // Code Goes Here
+
+
+})(jQuery);
